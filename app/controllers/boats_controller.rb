64a7d527d665fa0661027e930896ec2e28b0b340
mohcine chnahim <chnahim_6@hotmail.com>
2024-06-11 18:24:45 +0200

    vues boat dones

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,11 +1,17 @@
 class BoatsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @boats = Boat.all
   end
 
   def show
+    @boat = Boat.find(params[:id])
   end
 
   def new
+    @boat = Boat.new
   end
 
   def create
@@ -21,7 +27,7 @@ class BoatsController < ApplicationController
   end
 
   def my_boats
-
+  @boats = Boat.where(user_id: current_user)
   end
 
   # private
