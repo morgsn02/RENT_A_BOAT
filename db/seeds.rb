456f54fa39eadee304a36937114907bfda7a25c8
sonia chavepeyre <ciarasonia013@gmail.com>
2024-06-12 17:19:32 +0200

    photos bateaux

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ my_user = User.find(1)
 puts 'Creating 10 boats'
 10.times do
   boat = Boat.new(
-    name_boat:    Faker::Company.name,
+    name_boat:    Faker::Company.name, length: { maximum: 10 },
     price_per_day: rand(100..200),
     construction_year: rand(1980..2024),
     puissance: rand(100..300),
