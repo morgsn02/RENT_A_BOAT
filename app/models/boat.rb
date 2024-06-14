35ccb1080076c148228b3dcdadee56f1d180f445
mohcine chnahim <chnahim_6@hotmail.com>
2024-06-14 16:34:50 +0200

    front end

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,4 +3,6 @@ class Boat < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
   has_one_attached :photo
+  include PgSearch::Model
+  pg_search_scope :search_by_name, against: [:name_boat], using: { tsearch: { prefix: true } }
 end
